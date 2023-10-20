4fd2ad683120bd450726febe3e6f848c754764a7
Utkarsh Singh <utkarshsinghcbse92@gmail.com>
2023-10-20 20:32:52 +0530

    rules of game

---
+++ rule_of_game.js
@@ -0,0 +1,6 @@
+// Player-1 starts the Game
+// Player who score 100 points first ,wins the game.
+// if it holds then player changes  to player-2 and vice versa
+// if player does not hold and click on dice then the dice value will be added to his currentscore and he can click again on dice or click on hold
+// if 1 occur then his currntscore will be zero
+// if he holds then currenscore gets added to totalscore
